f5daec111e08cfd1e3d8a553bfa066e0bf9a5a2b
Jon Caleb <78839830+JCMullis1@users.noreply.github.com>
2022-05-24 16:20:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
+import random
 
 bfSize = int(input('How big shall the battlefield be? The area will be your input squared:'))
-
+print('')
+global coords
+n = int(bfSize + 1)
+b = []
+b.extend(range(n))
+coords = list([b] * bfSize)
 
 # call y before x I think it matters
 
-
 # defining players
 class Shinobi:
-    def __init__(self, name=str, element=str, health=int, chakra=int, choice=str):
+    def __init__(self, name=str, element=str, health=int, chakra=int, choice=str, speed=random.randint(1, 100)):
         self.name = name
         self.element = element
         self.health = health
         self.chakra = chakra
         self.choice = choice
+        self.speed = speed
 
     # this just defines the players used in the demo of the game
     @staticmethod
     def setplayers():
         global player, choice, p
         playuhs = int((input('How many players will be fighting today?:')))
-        print(f"there will be {playuhs} players")
         playhealth = int(input('how much health will each player have?:'))
         playchakra = int(input('and how much chakra will each player have?'))
+        print('')
         y = 0
         p = 1
 
@@ -29,6 +35,7 @@ class Shinobi:
             player = f"player{p}"
             globals()[player] = Shinobi(name=str(input('enter name:')), element=str(input('enter element:')),
                                         health=playhealth, chakra=playchakra, choice=str)
+            print('')
             p += 1
             y += 1
 
@@ -39,209 +46,72 @@ class Bout:
         Bout.x = x
         Bout.y = y
 
-    global letterChoice
-    letterChoice = []
+    global turnList
+    turnList = []
 
     @staticmethod
     def chakraloss():
-        global letterChoice, player
+        global turnList, player
 
     # picks an element and adds it to a list used for figuring out who won the game
     @staticmethod
     def pickleaction():
-        global selectedAction, letterChoice, player, choice, p
-
-        # this just checks the answer given in the round method, depending on the answer it changes the options given
-        # to the player
-        if len(letterChoice) == 2:
-            if letterChoice[1] == 'A':
-                print("Choose your attack!")
-                print("(water) (earth) (fire) (wind) (lightning) (attack)")
-                selectedAction = str((input("enter here:")))
-                player2.choice = str(selectedAction)
-                return player2.choice
-
-            if letterChoice[1] == 'B':
-                print("Choose your defense!")
-                print("(block) (dodge) (counter)")
-                selectedAction = str((input("enter here:")))
-                player2.choice = str(selectedAction)
-                return player2.choice
-
-            if letterChoice[1] == 'C':
-                print("Choose your special action!")
-                print("(summon) (guard break)")
-                selectedAction = str((input("enter here:")))
-                player2.choice = str(selectedAction)
-                return player2.choice
+        global selectedAction, turnList, player, choice, p
+        elementString = "(water) (earth) (fire) (wind) (lightning) (attack) (block) (dodge) (counter) (guard " \
+                        "break) (summon)"
+        # action selection
+        if len(turnList) == 2:
+            print("Choose your action!")
+            print(elementString)
+            selectedAction = (input("enter here:").strip())
+            player2.choice = selectedAction
+            return player2.choice
         else:
-            if letterChoice[0] == 'A':
-                print("Choose your attack!")
-                print("(water) (earth) (fire) (wind) (lightning) (attack)")
-                selectedAction = str((input("enter here:")))
-                player1.choice = str(selectedAction)
-                return player1.choice
-
-            if letterChoice[0] == 'B':
-                print("Choose your defense!")
-                print("(block) (dodge) (counter)")
-                selectedAction = str((input("enter here:")))
-                player1.choice = str(selectedAction)
-                return player1.choice
-
-            if letterChoice[0] == 'C':
-                print("Choose your special action!")
-                print("(summon) (guard break)")
-                selectedAction = str((input("enter here:")))
-                player1.choice = str(selectedAction)
-                return player1.choice
+            print("Choose your action!")
+            print(elementString)
+            selectedAction = (input("enter here:").strip())
+            player1.choice = selectedAction
+            return player1.choice
 
     # compares the choices of the players to find a suitable output
     @staticmethod
     def rps():
         global z, player, choice, p
         z = 0
+        rpsdict = {
+            "water": ["fire", "attack", "block", "counter", "guard break"],
+            "fire": ["wind", "attack", "block", "counter", "guard break"],
+            "wind": ["lightning", "attack", "block", "counter", "guard break"],
+            "lightning": ["earth", "attack", "block", "counter", "guard break"],
+            "earth": ["water", "attack", "block", "counter", "guard break"],
+            "counter": ["attack", "dodge"],
+            "block": ["attack", "counter"],
+            "attack": ["guard break", "dodge"],
+            "dodge": ["water", "fire", "wind", "lightning", "earth"],
+            "guard break": ["counter", "block", "dodge"]
+        }
 
-        # element checks
-        if player1.choice == 'water' and player2.choice == 'fire':
+        if player2.choice in rpsdict[player1.choice]:
             z += 1
-        elif player1.choice == 'fire' and player2.choice == 'water':
+            return
+        if player1.choice in rpsdict[player2.choice]:
             z -= 1
-        elif player1.choice == 'fire' and player2.choice == 'wind':
-            z += 1
-        elif player1.choice == 'wind' and player2.choice == 'fire':
-            z -= 1
-        elif player1.choice == 'wind' and player2.choice == 'lightning':
-            z += 1
-        elif player1.choice == 'lightning' and player2.choice == 'wind':
-            z -= 1
-        elif player1.choice == 'lightning' and player2.choice == 'earth':
-            z += 1
-        elif player1.choice == 'earth' and player2.choice == 'lightning':
-            z -= 1
-        elif player1.choice == 'earth' and player2.choice == 'water':
-            z += 1
-        elif player1.choice == 'water' and player2.choice == 'earth':
-            z -= 1
-
-        # basic attack checks
-        elif player1.choice == 'water' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'water':
-            z -= 1
-        elif player1.choice == 'fire' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'fire':
-            z -= 1
-        elif player1.choice == 'wind' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'wind':
-            z -= 1
-        elif player1.choice == 'lightning' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'lightning':
-            z -= 1
-        elif player1.choice == 'earth' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'earth':
-            z -= 1
-        elif player1.choice == 'block' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'block':
-            z -= 1
-        elif player1.choice == 'counter' and player2.choice == 'attack':
-            z += 1
-        elif player1.choice == 'attack' and player2.choice == 'counter':
-            z -= 1
-
-        # block checks
-        elif player1.choice == 'water' and player2.choice == 'block':
-            z += 1
-        elif player1.choice == 'block' and player2.choice == 'water':
-            z -= 1
-        elif player1.choice == 'earth' and player2.choice == 'block':
-            z += 1
-        elif player1.choice == 'block' and player2.choice == 'earth':
-            z -= 1
-        elif player1.choice == 'fire' and player2.choice == 'block':
-            z += 1
-        elif player1.choice == 'block' and player2.choice == 'fire':
-            z -= 1
-        elif player1.choice == 'wind' and player2.choice == 'block':
-            z += 1
-        elif player1.choice == 'block' and player2.choice == 'wind':
-            z -= 1
-        elif player1.choice == 'lightning' and player2.choice == 'block':
-            z += 1
-        elif player1.choice == 'block' and player2.choice == 'lightning':
-            z -= 1
-
-        # dodge checks
-        elif player1.choice == 'water' and player2.choice == 'dodge':
-            z -= 1
-        elif player1.choice == 'dodge' and player2.choice == 'water':
-            z += 1
-        elif player1.choice == 'earth' and player2.choice == 'dodge':
-            z -= 1
-        elif player1.choice == 'dodge' and player2.choice == 'earth':
-            z += 1
-        elif player1.choice == 'fire' and player2.choice == 'dodge':
-            z -= 1
-        elif player1.choice == 'dodge' and player2.choice == 'fire':
-            z += 1
-        elif player1.choice == 'wind' and player2.choice == 'dodge':
-            z -= 1
-        elif player1.choice == 'dodge' and player2.choice == 'wind':
-            z += 1
-        elif player1.choice == 'lightning' and player2.choice == 'dodge':
-            z -= 1
-        elif player1.choice == 'dodge' and player2.choice == 'lightning':
-            z += 1
-
-        # counter checks
-        elif player1.choice == 'counter' and player2.choice == 'water':
-            z -= 1
-        elif player1.choice == 'water' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'fire':
-            z -= 1
-        elif player1.choice == 'fire' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'wind':
-            z -= 1
-        elif player1.choice == 'wind' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'lightning':
-            z -= 1
-        elif player1.choice == 'lightning' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'earth':
-            z -= 1
-        elif player1.choice == 'earth' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'block':
-            z -= 1
-        elif player1.choice == 'wind' and player2.choice == 'counter':
-            z += 1
-        elif player1.choice == 'counter' and player2.choice == 'block':
-            z -= 1
-        elif player1.choice == 'block' and player2.choice == 'counter':
-            z += 1
-
-        # print(z) this print statement is used for debugging
-        return
+            return
+        else:
+            return
 
     # wincon checks the win condition 'z' to see who won the match
     @staticmethod
     def wincon():
         global player, choice, p, z
+        actionString = f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}"
 
         if z == 1:
             # print(z) debugging
             print(''
                   '')
             print(f"{player1.name} has won the round!")
-            print(f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}")
+            print(actionString)
 
             if player1.element == player1.choice:
                 player2.health -= 20
@@ -260,7 +130,7 @@ class Bout:
             print(''
                   '')
             print(f"{player2.name} has won the round!")
-            print(f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}")
+            print(actionString)
 
             if player2.element == player2.choice:
                 player1.health -= 20
@@ -279,7 +149,7 @@ class Bout:
                 print(''
                       '')
                 print('The round is undecided!')
-                print(f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}")
+                print(actionString)
 
                 print('But nothing happened.')
 
@@ -291,7 +161,7 @@ class Bout:
                 print(''
                       '')
                 print('The round is undecided!')
-                print(f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}")
+                print(actionString)
 
                 player1.health -= 10
                 player2.health -= 10
@@ -304,25 +174,19 @@ class Bout:
     @staticmethod
     def round():
         Shinobi.setplayers()
-        global player, letterChoice, p, choice
+        global player, turnList, p, choice, health, name
 
         while player1.health or player2.health > 0:
-            if len(letterChoice) == 2:
-                letterChoice.clear()
+            if len(turnList) == 2:
+                turnList.clear()
             print(f"{player1.name}\'s turn")
-            print("pick your action")
-            print("(A for attack options) or (B for defensive options) or (C for special options)")
-            answer = str(input('enter here:'))
-            letterChoice.append(answer)
+            turnList.append(0)
             Bout.pickleaction()
 
             print('')
 
             print(f"{player2.name}\'s turn")
-            print("pick your action")
-            print("(A for attack options) or (B for defensive options) or (C for special options)")
-            answer2 = str(input('enter here:'))
-            letterChoice.append(answer2)
+            turnList.append(0)
             Bout.pickleaction()
             Bout.rps()
             Bout.wincon()
@@ -336,13 +200,6 @@ class Bout:
             else:
                 continue
 
-
-# making the dynamic matrix
-n = int(bfSize+1)
-b = []
-b.extend(range(n))
-a = list([b]*bfSize)
-print(a)
 
 Bout.round()
 
