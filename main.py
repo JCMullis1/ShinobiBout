8054e25934f1e1bd78e5a232f692c65e66a3ddb0
Jon Caleb <78839830+JCMullis1@users.noreply.github.com>
2022-06-08 15:58:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,214 +1,118 @@
-import random
-
-global n, coords
-bfSize = int(input('How big shall the battlefield be? The area will be your input squared:'))
+bfSize = int(input('How big shall the battlefield be? The area will be your input squared: '))
 print('')
-n = int(bfSize + 1)
-b = []
-b.extend(range(n))
-coords = list([b] * n)
+coords = [[None] * bfSize for _ in range(bfSize)]
+print(coords)
+
+targetxList = []
+targetyList = []
+playerList = []
+elementList = ["water", "fire", "wind" "lightning", "earth"]
+
+actiondict = {
+    "water": ["fire", "attack", "block", "counter", "guard break"],
+    "fire": ["wind", "attack", "block", "counter", "guard break"],
+    "wind": ["lightning", "attack", "block", "counter", "guard break"],
+    "lightning": ["earth", "attack", "block", "counter", "guard break"],
+    "earth": ["water", "attack", "block", "counter", "guard break"],
+    "counter": ["attack", "dodge"],
+    "block": ["attack", "counter"],
+    "attack": ["guard break", "dodge"],
+    "dodge": ["water", "fire", "wind", "lightning", "earth"],
+    "guard break": ["counter", "block", "dodge"]
+}
 
 
 # defining players
 class Shinobi:
-    def __init__(self, name=str, element=str, health=int, chakra=int,
-                 playX=int, playY=int, speed=int, choice=str):
+    def __init__(self, name=str, element=str, health=int, chakra=int, playerX=int, playerY=int, targetX=int,
+                 targetY=int, choice=str):
         self.name = name
         self.element = element
         self.health = health
         self.chakra = chakra
-        self.playX = playX
-        self.playY = playY
-        self.speed = speed
         self.choice = choice
+        self.playerX = playerX
+        self.playerY = playerY
+        self.targetX = targetX
+        self.targetY = targetY
+
+    @staticmethod
+    def placeplayers():
+        y = 0
+        x = 0
+        for ele in range(len(playerList)):
+            coords[x][y] = playerList[ele]
+            playerList[ele].playerX = x
+            playerList[ele].playerY = y
+            x += 1
+            if x >= bfSize:
+                x -= bfSize
+                y += 1
+            if y > bfSize:
+                print('there is too many players')
 
     # this just defines the players used in the demo of the game
     @staticmethod
     def setplayers():
-        global player, choice, p, bfSize
-        playuhs = int((input('How many players will be fighting today?:')))
-        playhealth = int(input('how much health will each player have?:'))
-        playchakra = int(input('and how much chakra will each player have?'))
+        global player, playuhs
+        playuhs = int((input('How many players will be fighting today?: ')))
+        playhealth = int(input('How much health will each player have?: '))
+        playchakra = int(input('How much chakra will each player have?: '))
         print('')
-        y = 0
-        p = 1
-        bf = 0
-        bfp = n/2
-
-        while y != playuhs:
-            player = f"player{p}"
-            globals()[player] = Shinobi(name=str(input('enter name:')), element=str(input('enter element:')),
-                                        health=playhealth, chakra=playchakra, playX=int(bfp + bf), playY=int(bfp + bf),
-                                        speed=random.randint(1, 100), choice=str)
+        playID = 0
+        for number in range(playuhs):
+            player = f"player{playID}"
+            globals()[player] = Shinobi(name=str(input('Enter name: ')), element=str(input('Enter element: ')),
+                                        health=playhealth, chakra=playchakra, playerX=int, playerY=int, targetX=int,
+                                        targetY=int, choice=str)
+            playerList.append(globals()[player])
             print('')
-            bf += 1
-            p += 1
-            y += 1
+            playID += 1
 
 
-global turnList
-turnList = []
-
-
-def coordcheck():
-    global coords
-
-
-
-def chakraloss():
-    global turnList, player
-
-    # picks an element and adds it to a list used for figuring out who won the game
-
-
-def pickleaction():
-    global selectedAction, turnList, player, choice, p
+Shinobi.setplayers()
+# game loop
+global playuhs
+Shinobi.placeplayers()
+print(coords)
+while True:
     elementString = "(water) (earth) (fire) (wind) (lightning) (attack) (block) (dodge) (counter) (guard " \
                     "break) (summon)"
-    # action selection
-    if len(turnList) == 2:
+    # turn system
+    for i in range(len(playerList)):
+        print(playerList[i].name + f"'s turn")
         print("Choose your action!")
         print(elementString)
-        selectedAction = (input("enter here:").strip())
-        player2.choice = selectedAction
-        return player2.choice
-    else:
-        print("Choose your action!")
-        print(elementString)
-        selectedAction = (input("enter here:").strip())
-        player1.choice = selectedAction
-        return player1.choice
-
-    # compares the choices of the players to find a suitable output
-
-
-def rps():
-    global z, player, choice, p
-    z = 0
-    rpsdict = {
-        "water": ["fire", "attack", "block", "counter", "guard break"],
-        "fire": ["wind", "attack", "block", "counter", "guard break"],
-        "wind": ["lightning", "attack", "block", "counter", "guard break"],
-        "lightning": ["earth", "attack", "block", "counter", "guard break"],
-        "earth": ["water", "attack", "block", "counter", "guard break"],
-        "counter": ["attack", "dodge"],
-        "block": ["attack", "counter"],
-        "attack": ["guard break", "dodge"],
-        "dodge": ["water", "fire", "wind", "lightning", "earth"],
-        "guard break": ["counter", "block", "dodge"]
-    }
-
-    if player2.choice in rpsdict[player1.choice]:
-        z += 1
-        return
-    if player1.choice in rpsdict[player2.choice]:
-        z -= 1
-        return
-    else:
-        return
-
-
-# wincon checks the win condition 'z' to see who won the match
-def wincon():
-    global player, choice, p, z
-    actionString = f"{player1.name} chose {player1.choice} and {player2.name} chose {player2.choice}"
-
-    if z == 1:
-        # print(z) debugging
-        print(''
-              '')
-        print(f"{player1.name} has won the round!")
-        print(actionString)
-
-        if player1.element == player1.choice:
-            player2.health -= 20
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-        else:
-            player2.health -= 10
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-    elif z == -1:
-        # print(z) debugging
-        print(''
-              '')
-        print(f"{player2.name} has won the round!")
-        print(actionString)
-
-        if player2.element == player2.choice:
-            player1.health -= 20
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-        else:
-            player1.health -= 10
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-    else:
-        if player1.choice and player2.choice == 'block':
-            print(''
-                  '')
-            print('The round is undecided!')
-            print(actionString)
-
-            print('But nothing happened.')
-
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-        else:
-            print(''
-                  '')
-            print('The round is undecided!')
-            print(actionString)
-
-            player1.health -= 10
-            player2.health -= 10
-
-            print(f"{player1.name} is now at {player1.health} health! And {player2.name} is now at "
-                  f"{player2.health} health!")
-            print('')
-
-
-# game loop and turn system
-def round():
-    Shinobi.setplayers()
-    global player, turnList, p, choice, health, name
-
-    while player1.health or player2.health > 0:
-        if len(turnList) == 2:
-            turnList.clear()
-        print(f"{player1.name} is at x{player1.playX} y{player1.playY}")
-        print(f"{player2.name} is at x{player2.playX} y{player2.playY}")
+        playerList[i].choice = (input("enter here: ").strip())
+        if playerList[i].choice in elementList:
+            playerList[i].chakra -= 10
+        print("Where do you want to attack?")
+        for j in range(len(playerList)):
+            print(f"{playerList[j].name} is at x{playerList[j].playerX}  y{playerList[j].playerY}")
         print('')
-        print(f"{player1.name}\'s turn")
-        turnList.append(0)
-        pickleaction()
-
+        targetX = int(input("Enter x coordinate: "))
+        targetY = int(input("Enter y coordinate: "))
+        targetxList.append(targetX)
+        targetyList.append(targetY)
+        print('')
+        if playerList[i].choice in elementList:
+            playerList[i].chakra -= 10
         print('')
 
-        print(f"{player2.name}\'s turn")
-        turnList.append(0)
-        pickleaction()
-        rps()
-        wincon()
-
-        if player1.health < 2:
-            print(f"Game over! {player2.name} wins!")
-            break
-        elif player2.health < 2:
-            print(f"Game over! {player1.name} wins!")
-            break
-        else:
-            continue
-
-
-round()
+    tempList = []
+    for e in range(len(playerList)):
+        tempList.clear()
+        playerPosX = playerList[e].playerX
+        playerPosY = playerList[e].playerY
+        playerTarX = targetxList[e]
+        playerTarY = targetyList[e]
+        if coords[playerTarX][playerTarY] is not None:
+            tempList.append(coords[playerTarX][playerTarY])
+            if tempList[0].choice in actiondict[playerList[e].choice]:
+                # add the health losses and print statement abusing for the attacker winning
+                print('attacker won i think')
+            elif playerList[e].choice in actiondict[tempList[0].choice]:
+                # defending player won
+                print('defender won i think')
+            else:
+                print('nobody won or this didnt work')
