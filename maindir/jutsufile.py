43178464a92393664d5ca20b5049fbb5de899141
Jon Caleb <Joncalebmullis@gmail.com>
2022-06-11 22:43:48 -0500

    alpha version 0.7

--- maindir/jutsufile.py
+++ maindir/jutsufile.py
@@ -1,11 +1,18 @@
-
+jutsuList = []
 class Jutsu:
-    def __init__(self, dictkey=str, damage=int, atrange=int):
+    def __init__(self, name=str, dictkey=str, bloodline=str, damage=int, atrange=int, chakracost=float):
+        self.name = name
         self.dictkey = dictkey
+        self.bloodline = bloodline
         self.damage = damage
         self.atrange = atrange
-# jutsu list used for the main file
-fireball = Jutsu("fire", 20, 3)
-waterprison = Jutsu("water", 10, 1)
+        self.chakracost = damage*.9
 
+# jutsu list used for the main file
+fireball1 = Jutsu("fireball", "fire", "land of fire", 20, 3)
+jutsuList.append(fireball1)
+waterprison1 = Jutsu("waterprison", "water", "land of water", 10, 1)
+jutsuList.append(waterprison1)
+# create a method that creates jutsu for the ninja to use instead and the chakra cost is based on the damage that will obviously have a limit
+# and for summons they will have jutsu as well but less and they will be fun/funky
 
--- maindir/main.py
+++ maindir/main.py
@@ -1,3 +1,4 @@
+from maindir.jutsufile import *
 
 # battle field creation using matrix
 bfSize = int(input('How big shall the battlefield be? The area will be your input squared: '))
@@ -5,8 +6,7 @@ print('')
 coords = [[None] * bfSize for _ in range(bfSize)]
 print(coords)
 
-targetxList = []
-targetyList = []
+jutsuList1 = []
 playerList = []
 elementList = ["water", "fire", "wind" "lightning", "earth"]
 
@@ -20,24 +20,34 @@ actiondict = {
     "block": ["attack", "counter"],
     "attack": ["guard break", "dodge"],
     "dodge": ["water", "fire", "wind", "lightning", "earth"],
-    "guard break": ["counter", "block", "dodge"]
+    "guard break": ["counter", "block", "dodge"],
+    "move": ["block", "dodge"]
 }
 
-print()
+move1 = Jutsu("move", "move", str, 0, 3)
+guardbreak1 = Jutsu("guard break", "guard break", str, 10, 1)
+counter1 = Jutsu("counter", "counter", str, 10, 1)
+dodge1 = Jutsu("dodge", "dodge", str, 10, 1)
+block1 = Jutsu("block", "block", str, 10, 1)
+
+rlist = [move1, block1, guardbreak1, counter1, dodge1]
+
 # defining players
 class Shinobi:
     def __init__(self, name=str, element=str, health=int, chakra=int, playerX=int, playerY=int, targetX=int,
-                 targetY=int, tired=False, choice=str):
+                 targetY=int, tired=False, jutsu=list, bline=str, choice=Jutsu):
         self.name = name
         self.element = element
         self.health = health
         self.chakra = chakra
-        self.choice = choice
         self.playerX = playerX
         self.playerY = playerY
         self.targetX = targetX
         self.targetY = targetY
         self.tired = tired
+        self.jutsu = jutsu
+        self.bline = bline
+        self.choice = choice
 
     # places players on the map
     @staticmethod
@@ -54,6 +64,7 @@ class Shinobi:
                 y += 1
             if y > bfSize:
                 print('there is too many players')
+                break
 
     # this just defines the players used in the demo of the game
     @staticmethod
@@ -67,18 +78,22 @@ class Shinobi:
         for number in range(playuhs):
             player = f"player{playID}"
             globals()[player] = Shinobi(name=str(input('Enter name: ')), element=str(input('Enter element: ')),
-                                        health=playhealth, chakra=playchakra, playerX=int, playerY=int, targetX=int,
-                                        targetY=int, tired=False, choice=str)
+                                        health=playhealth, chakra=playchakra, jutsu=list(rlist), bline=str(input('Enter bloodline: ')))
             playerList.append(globals()[player])
-            print('')
+            for juts in range(len(jutsuList)):
+                if playerList[playID].bline == jutsuList[juts].bloodline:
+                    playerList[playID].jutsu.append(jutsuList[juts])
+            customjutsu = int(input("How many custom jutsu would you like to make?: "))
+            for cus in range(customjutsu):
+                jutsuID = 0
+                jutsu = f"jutsu{jutsuID}"
+                globals()[jutsu] = Jutsu(name=str(input("Enter jutsu name: ")), dictkey=str(input("Enter jutsu type (element or type of action): ")),
+                                         bloodline=playerList[playID].bline, damage=int(input("Enter damage of jutsu: ")),
+                                         atrange=int(input("Enter range of jutsu: ")))
+                globals()[player].jutsu.append(globals()[jutsu])
+                print('')
             playID += 1
-
-# class for jutsu
-class Jutsu:
-    def __init__(self, dictkey=str, damage=int, atrange=int):
-        self.dictkey = dictkey
-        self.damage = damage
-        self.atrange = atrange
+            print('')
 
 global playuhs
 Shinobi.setplayers()
@@ -86,10 +101,9 @@ Shinobi.placeplayers()
 # game loop
 running = True
 while running:
-    elementString = "(water) (earth) (fire) (wind) (lightning) (attack) (block) (dodge) (counter) (guard " \
-                    "break) (summon)"
     # turn system
     for i in range(len(playerList)):
+        # player death and game over check
         if playerList[i].health <= 0:
             print(f"It seems {playerList[i].name} has died in combat and can no longer fight...")
             print('')
@@ -100,41 +114,62 @@ while running:
                 running = False
                 exit(0)
             continue
+
         playerList[i].tired = False
-        print(playerList[i].name + f"'s turn")
-        print(f"You have {playerList[i].chakra} chakra")
-        print("Choose your action!")
-        print(elementString)
-        playerList[i].choice = (input("enter here: ").strip())
-        if playerList[i].choice == "move":
-            # moving not added
-            pass
+        print(f'''
+    {playerList[i].name}'s turn
+        You have {playerList[i].chakra} chakra
+            Choose your action!
+        ''')
+        for g in range(len(playerList[i].jutsu)):
+            print(f"{playerList[i].jutsu[g].name}")
+        x = str(input("Enter here: ")).strip()
+        for y in range(len(playerList[i].jutsu)):
+            if x == playerList[i].jutsu[y].name:
+                playerList[i].choice = playerList[i].jutsu[y]
+
+        if playerList[i].choice.dictkey == "move":
+            a = int(input("How many units do you want to move?: ").strip())
+            print("Which direction?")
+            print("(up ~ y + input) (down ~ y - input) (left ~ x - input) (right ~ x + input)")
+            d = str(input("enter here: ")).strip()
+            if d == "up":
+                playerList[i].playerY += a
+            if d == "down":
+                playerList[i].playerY -= a
+            if d == "right":
+                playerList[i].playerX += a
+            if d == "left":
+                playerList[i].playerX -= a
+            if playerList[i].playerX < 0:
+                playerList[i].playerX = 0
+            if playerList[i].playerY < 0:
+                playerList[i].playerY = 0
+
         print("Where do you want to attack?")
+        print('')
         for j in range(len(playerList)):
             print(f"{playerList[j].name} is at x{playerList[j].playerX}  y{playerList[j].playerY}")
-        print('')
-        playerList[i].targetX = int(input("Enter x coordinate: "))
-        playerList[i].targetY = int(input("Enter y coordinate: "))
-        targetxList.append(playerList[i].targetX)
-        targetyList.append(playerList[i].targetY)
-
+        playerList[i].targetX = int(input("Enter x coordinate: ").strip())
+        playerList[i].targetY = int(input("Enter y coordinate: ").strip())
+        if playerList[i].playerX - playerList[i].targetX >= playerList[i].choice.atrange and playerList[i].playerY - playerList[i].targetY >= playerList[i].choice.atrange:
+            print('this works')
+            pass
         # add a check to see if attack is out of range
 
         print('')
-        if playerList[i].choice in elementList:
-            if playerList[i].chakra <= 0:
-                print('Not enough chakra')
-                playerList[i].choice = "block"
-                continue
-            else:
-                playerList[i].chakra -= 10
+        if playerList[i].choice.dictkey in elementList and playerList[i].chakra >= float(playerList[i].choice.damage*.9):
+            playerList[i].chakra -= float(playerList[i].choice.damage*.9)
+        elif playerList[i].choice.dictkey in elementList and playerList[i].chakra < float(playerList[i].choice.damage*.9):
+            print('Not enough chakra')
+            playerList[i].choice = playerList[i].jutsu[1]
+            # changes player action to block if they don't have enough chakra
         print('')
 
-    # turn execution
+        # turn execution
     tempList = []
     for e in range(len(playerList)):
         tempList.clear()
-
         if coords[playerList[e].targetX][playerList[e].targetY] is not None:
             tempList.append(coords[playerList[e].targetX][playerList[e].targetY])
             if coords[tempList[0].targetX][tempList[0].targetY] == coords[playerList[e].playerX][playerList[e].playerY]:
@@ -143,59 +178,55 @@ while running:
             if not playerList[e].tired:
 
                 # attacker won
-                if tempList[0].choice in actiondict[playerList[e].choice]:
-                    if playerList[e].choice == playerList[e].element:
-                        tempList[0].health -= 20
-                        print(f"{playerList[e].name} has won the bout!")
-                        print(
-                            f"{playerList[e].name} chose {playerList[e].choice}, and {tempList[0].name} chose {tempList[0].choice}!")
-                        print(
-                            f"{playerList[e].name} is now at {playerList[e].health}, and {tempList[0].name} is at {tempList[0].health}!")
-                        print('')
+                if tempList[0].choice.dictkey in actiondict[playerList[e].choice.dictkey]:
+                    if playerList[e].choice.dictkey == playerList[e].element:
+                        tempList[0].health -= float(playerList[e].choice.damage*1.5)
+                        print(f'''
+                        {playerList[e].name} has won the bout!
+                        {playerList[e].name} chose {playerList[e].choice.name}, and {tempList[0].name} chose {tempList[0].choice.name}!
+                        {playerList[e].name} is now at {playerList[e].health}, and {tempList[0].name} is at {tempList[0].health}!
+                        ''')
 
                     else:
-                        tempList[0].health -= 10
-                        print(f"{playerList[e].name} has won the bout!")
-                        print(
-                            f"{playerList[e].name} chose {playerList[e].choice}, and {tempList[0].name} chose {tempList[0].choice}!")
-                        print(
-                            f"{playerList[e].name} is now at {playerList[e].health}, and {tempList[0].name} is at {tempList[0].health}!")
-                        print('')
+                        tempList[0].health -= float(playerList[e].choice.damage)
+                        print(f'''
+                        {playerList[e].name} has won the bout!
+                        {playerList[e].name} chose {playerList[e].choice.name}, and {tempList[0].name} chose {tempList[0].choice.name}!
+                        {playerList[e].name} is now at {playerList[e].health}, and {tempList[0].name} is at {tempList[0].health}!
+                        ''')
                 # attacker won
 
                 # defender won
-                elif playerList[e].choice in actiondict[tempList[0].choice]:
-                    if tempList[0].choice == tempList[0].element:
-                        playerList[e].health -= 20
-                        print(f"{tempList[0].name} has won the bout!")
-                        print(
-                            f"{tempList[0].name} chose {tempList[0].choice}, and {playerList[e].name} chose {playerList[e].choice}!")
-                        print(
-                            f"{tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!")
-                        print('')
+                elif playerList[e].choice.dictkey in actiondict[tempList[0].choice.dictkey]:
+                    if tempList[0].choice.dictkey == tempList[0].element:
+                        playerList[e].health -= float(tempList[0].choice.damage*1.5)
+                        print(f'''
+                        {tempList[0].name} has won the bout!
+                        {tempList[0].name} chose {tempList[0].choice.name}, and {playerList[e].name} chose {playerList[e].choice.name}!
+                        {tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!
+                        ''')
 
                     else:
-                        playerList[e].health -= 10
-                        print(f"{tempList[0].name} has won the bout!")
-                        print(
-                            f"{tempList[0].name} chose {tempList[0].choice}, and {playerList[e].name} chose {playerList[e].choice}!")
-                        print(
-                            f"{tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!")
-                        print('')
+                        playerList[e].health -= float(tempList[0].choice.damage)
+                        print(f'''
+                        {tempList[0].name} has won the bout!
+                        {tempList[0].name} chose {tempList[0].choice.name}, and {playerList[e].name} chose {playerList[e].choice.name}!
+                        {tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!
+                        ''')
                 # defender won
 
                 # nobody won
                 else:
-                    if tempList[0].choice and playerList[e].choice == "block":
+                    if tempList[0].choice.dictkey == "block" and playerList[e].choice.dictkey == "block":
                         print("Both Shinobi blocked so nothing happened!")
                     else:
-                        playerList[e].health -= 10
-                        tempList[0].health -= 10
-                        print("The bout ended in stalemate!")
-                        print(
-                            f"{playerList[e].name} chose {playerList[e].choice}, and {tempList[0].name} chose {tempList[0].choice}!")
-                        print(
-                            f"{tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!")
+                        playerList[e].health -= float(tempList[0].choice.damage)
+                        tempList[0].health -= float(playerList[e].choice.damage)
+                        print(f'''
+                        The bout ended in stalemate!
+                        {playerList[e].name} chose {playerList[e].choice.name}, and {tempList[0].name} chose {tempList[0].choice.name}!
+                        {tempList[0].name} is now at {tempList[0].health}, and {playerList[e].name} is at {playerList[e].health}!
+                        ''')
                 # nobody won
 
             elif playerList[e].tired:
